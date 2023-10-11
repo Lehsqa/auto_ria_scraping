4f1ebe2f0adad9e304946ca76246c284e9214265
devRSV <devRSV@d.c>
2023-10-11 19:00:19 +0300

    Return original main.py

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ from dump.dump import run_dump
 if __name__ == '__main__':
     s = Scraper()
 
-    s.run()
-    # schedule.every().day.at('12:00').do(s.run)
-    # schedule.every().day.at('00:00').do(run_dump)
+    schedule.every().day.at('12:00').do(s.run)
+    schedule.every().day.at('00:00').do(run_dump)
 
-    # while True:
-    #     schedule.run_pending()
-    #     sleep(1)
+    while True:
+        schedule.run_pending()
+        sleep(1)
